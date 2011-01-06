b4567fd6f7bffd4ac8e8c47f577c5c73719f7e57
Virtuo <pierre@ruyssen.fr>
2011-01-06 15:58:59 +0100

    Add info in setup.py file.

--- setup.py
+++ setup.py
@@ -7,16 +7,25 @@ setup(
   name=PACKAGE,
   version=VERSION,
   description='AuthServer plugin for Trac',
-  license='Trac license',
+  keywords='authserver trac oauth2 authentication authorization',
+  license='Trac license (modified BSD)',
   author='Pierre Ruyssen (AF83)',
-  author_email='pierre@ruyssen.fr',
-  url='',
+  author_email='pierre@ruyssen.eu',
+  url='https://github.com/AF83/trac_auth_server',
   packages=['trac_auth_server'],
   entry_points={'trac.plugins': '%s = trac_auth_server' % PACKAGE},
   include_package_data = True,
   package_data={},
   install_requires = [
     'AuthServerClient==0.0.1'
+  ],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Software Development :: Bug Tracking",
   ],
 )
 
