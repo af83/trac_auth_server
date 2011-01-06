b23458e82028c4835cafb2edd2008e0b7439732f
Virtuo <pierre@ruyssen.fr>
2011-01-06 13:02:47 +0100

    NoAnonymousAuthServer plugin.

--- trac_auth_server/trac_auth_server.py
+++ trac_auth_server/trac_auth_server.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from genshi.builder import tag
+from trac.core import Component, implements
 from trac.web import HTTPBadRequest, HTTPUnauthorized, HTTPInternalError
+from trac.web.api import IRequestFilter
 from trac.web.auth import LoginModule
 
 from auth_server_client import oauth2
@@ -15,11 +17,29 @@ oauth2.init("4d2302d52b5ee74918000009",
             "http://example.com:7070/auth")
 
 
+class NoAnonymousAuthServerPlugin(Component):
+  """Redirects user to auth_server when not logged-in.
+  """
+  implements(IRequestFilter)
+
+  # IRequestFilter methods
+  def pre_process_request(self, req, handler):
+    if req.path_info.startswith('/auth_server_') or req.authname != 'anonymous':
+      return handler
+    state = req.abs_href() + req.path_info
+    url = oauth2.get_login_url(state)
+    req.redirect(url)
+
+  def post_process_request(self, req, template, data, content_type):
+    return template, data, content_type
+
+
 class AuthServerPlugin(LoginModule):
   """Enables an user to log-in using auth_server.
   """
 
   # INavigationContributor methods
+  # To Add a link to log-in using auth_server
   def get_active_navigation_item(self, req):
     return 'auth_server_login'
 
@@ -28,7 +48,9 @@ class AuthServerPlugin(LoginModule):
       yield ('metanav', 'login', 
              tag.a(('AuthServer Login'), href=req.href.auth_server_login()))
 
+
   # IRequestHandler methods
+  # To handle login / logout / process
   def match_request(self, req):
     return req.path_info in ['/auth_server_login', '/auth_server_process'] or \
            LoginModule.match_request(self, req)
@@ -66,7 +88,7 @@ class AuthServerPlugin(LoginModule):
     try:
       info = oauth2.get_authorizations(access_token)
     except ValueError, err:
-      raise HTTPInternalError('Bad answer from auth_server: %s' % err)
+      raise HTTPInternalError(str(err))
 
     print "info:", info
     # TODO: 403 if not good authorizations
